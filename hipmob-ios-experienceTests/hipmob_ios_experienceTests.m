d80c7675939cc4913b971d2ac0efdca58497273e
Femi Omojola <femi@hipmob.com>
2014-10-08 12:07:15 -0400

    Added the missing test files

---
+++ hipmob-ios-experienceTests/hipmob_ios_experienceTests.m
@@ -0,0 +1,34 @@
+//
+//  hipmob_ios_experienceTests.m
+//  hipmob-ios-experienceTests
+//
+//  Created by Olufemi Omojola on 1/8/14.
+//  Copyright (c) 2014 Orthogonal Labs, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hipmob_ios_experienceTests : XCTestCase
+
+@end
+
+@implementation hipmob_ios_experienceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
